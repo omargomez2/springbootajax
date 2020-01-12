b52675f6f820dca5bbfd7158aa2c91567d016e52
OG <omargomez2@gmail.com>
2020-01-11 23:37:44 -0500

    v1

---
+++ ajaxrestclient/src/main/java/net/springboot/ajaxrestclient/AjaxrestclientApplication.java
@@ -0,0 +1,13 @@
+package net.springboot.ajaxrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxrestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxrestclientApplication.class, args);
+	}
+
+}
---
+++ ajaxrestclient/src/test/java/net/springboot/ajaxrestclient/AjaxrestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot.ajaxrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxrestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
